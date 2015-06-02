af37596b9ad102ded18d72331e64fabe4e571216
Caroline Jardim Siqueira <carolinejs@iMac-de-Caroline.local>
2015-06-02 15:33:25 -0300

    fixed bug

--- cardapio/views.py
+++ cardapio/views.py
@@ -361,7 +361,7 @@ def prep_cardapio(request,id_dia_cardapio):
 
 	if request.method == 'POST' and 'add' in request.POST:
 		f_prep = request.POST.get('prep')
-		card = Cardapio_Prep(prep_id=f_prep,dia_id=id_dia_cardapio,r1=0,r2=0,r3_1=0,r3_2=0,r4=0,r5=0,r6=0,r7=0,r8=0,r9=0,r10=0)
+		card = Cardapio_Prep(prep_id=f_prep,dia_id=id_dia_cardapio,r1=0,r2=0,r3=0,r4=0,r5=0,r6=0,r7=0,r8=0,r9=0,r10=0)
 		card.save()
 		return redirect('/prep_cardapio/' + id_dia_cardapio)
 
